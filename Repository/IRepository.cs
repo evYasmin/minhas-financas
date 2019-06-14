699f3a810ab7305ec6d65529d0edf26be7cb15c4
evYasmin <49412927+evYasmin@users.noreply.github.com>
2019-06-14 14:28:15 -0300

    Criado a class e feito o IRepository

---
+++ Repository/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    interface IRepository
+    {
+        int Inserir(ContasPagar contasPagar);
+
+        bool Apagar(int id);
+
+        bool Atualizar(ContasPagar contasPagar);
+
+        ContasPagar ObterPeloId(int id);
+
+        List<ContasPagar> ObterTodos(string busca);
+
+
+    }
+}
