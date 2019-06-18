6045f5e56370d467f24beefe3bd5e6352562f841
evYasmin <49412927+evYasmin@users.noreply.github.com>
2019-06-18 16:59:37 -0300

    IRepository Contas a receber criado e feito.

---
+++ Repository/IRepositoryContaReceber.cs
@@ -0,0 +1,22 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    interface IRepositoryContaReceber
+    {
+        int Inserir(ContasReceber contasReceber);
+
+        bool Apagar(int id);
+
+        bool Atualizar(ContasReceber contasReceber);
+
+        ContasReceber ObterPeloId(int id);
+
+        List<ContasReceber> ObterTodos(string busca);
+    }
+}
