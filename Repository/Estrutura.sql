9729c45c591b8243d830273b1afd5a60b52f2b26
evYasmin <49412927+evYasmin@users.noreply.github.com>
2019-06-18 16:47:05 -0300

    Criado a estrutura de Contas a receber

--- Repository/Estrutura.sql
+++ Repository/Estrutura.sql
@@ -10,3 +10,13 @@ status VARCHAR(100)
 
 
 SELECT * FROM contasPagar;
+
+
+CREATE TABLE contasReceber (
+id INT PRIMARY KEY IDENTITY(1,1) , 
+nome VARCHAR(100) ,
+valor DECIMAL (8,2),
+tipo VARCHAR(100) , 
+descricao VARCHAR(100) , 
+status VARCHAR(100) 
+);
