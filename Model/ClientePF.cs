93cc233ba16fc96643a7e2c3efaec13c92b4730c
evYasmin <49412927+evYasmin@users.noreply.github.com>
2019-06-19 15:26:44 -0300

    Feito o model ClientePF.

---
+++ Model/ClientePF.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class ClientePF
+    {
+        public int id;
+        public string nome;
+        public string cpf;
+        public string dataNascimento;
+        public string rg;
+        public string sexo;
+    }
+}
